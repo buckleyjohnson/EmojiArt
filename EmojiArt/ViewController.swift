cf211c4fa1f98fade069bfa8a99dfd4a948678ef
Buckley Johnson <buck@qksee.com>
2019-11-09 10:15:08 -0700

    Initial Commit

---
+++ EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiArt
+//
+//  Created by buckley johnson on 11/9/19.
+//  Copyright © 2019 buckley johnson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
